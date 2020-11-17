8ee89cc619c1d191127a96d5b1acd80697806088
MattiaRacca <mattia.rh@gmail.com>
2020-11-17 18:52:09 +0100

    more fixing for the pavilion

--- rice.sh
+++ rice.sh
@@ -17,6 +17,7 @@ read -p 'Do you want i3gaps? [y/n]: ' answer
 if [ "$answer" = "y" -o -z "$answer" ];then
   sudo add-apt-repository ppa:kgilmer/speed-ricer
   sudo apt update && sudo apt install i3-gaps
+  i3-config-wizard
 fi
 
 if [ $DESKTOP_SESSION == "i3" ]; then
@@ -35,14 +36,21 @@ if [ $DESKTOP_SESSION == "i3" ]; then
   sudo apt install polybar
   # urvxt
   sudo apt install rxvt-unicode
+  # mpv
+  sudo apt intsall mpv
   # dunst
   firefox https://github.com/dunst-project/dunst
+  # networkmanager_dmenu
+  firefox https://github.com/firecat53/networkmanager-dmenu
   # install icomoon feather font
   sudo gnome-font-viewer ~/dotfiles/polybar/.config/polybar/fontsicomoon-feather.ttf
   echo -e "\n===== Now you can stow the related stuff! =====\n"
   # install font awesome 5
   firefox https://github.com/FortAwesome/Font-Awesome
   echo -e "Use gnome-font-viewer as for the icomoon feather font"
+  # To change fonts and GTK
   echo -e "Use lxappearance for changing GTK theme and icons"
   echo -e "Remember to set Wallpaper.png and Profile.png in ~/Pictures"
+  # if xbacklight doesn't work, there is light
+  firefox https://github.com/haikarainen/light
 fi
