855957f5887840fb490e266e30fe5dd24850a705
MattiaRacca <mattia.rh@gmail.com>
2024-09-17 22:15:20 +0200

    preparing for ngix, moving pihole webserver to 8080

--- host_specific/raspberrypi.sh
+++ host_specific/raspberrypi.sh
@@ -1,3 +1,4 @@
 
 # temperature of CPU -- one-liner
 alias cputemp='head -n 1 /sys/class/thermal/thermal_zone0/temp | xargs -I{} awk "BEGIN {printf \"%.2f\n\", {}/1000}"'
+alias start-mopidy='nohup mopidy -v >> /home/pi/.data/mopidy/mopidy.log 2>&1 &'
