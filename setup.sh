33f9453b58bd285d4e2158fffedf56f1a42c0137
MattiaRacca <mattia.rh@gmail.com>
2025-09-10 14:56:05 +0200

    correct pipe

--- setup.sh
+++ setup.sh
@@ -89,7 +89,7 @@ fi
 
 read -p 'Do you want miniconda? [y/n]: ' answer
 if [ "$answer" = "y" -o -z "$answer" ];then
-  wget -qO- https://repo.anaconda.com/miniconda/Miniconda3-latest-$(uname)-$(uname -m).sh | less
+  wget -qO- https://repo.anaconda.com/miniconda/Miniconda3-latest-$(uname)-$(uname -m).sh | bash
   stow conda
 fi
 
